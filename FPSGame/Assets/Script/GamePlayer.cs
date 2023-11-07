3d2bd7ca9f3227413aaa74ae3a70048dd4925c40
kjih1104 <kjih114@gmail.com>
2023-11-07 23:21:00 +0900

    Mirror Ready
    Mirror 활용을 위한 가장 기초셋팅
    코드 부분에 주석이 깨지면 알려줘

---
+++ FPSGame/Assets/Script/GamePlayer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+/*
+ * 게임 방에 들어갈 플레이어의 프리팹으로
+ * 이 곳에 플레이어의 움직임, HP등을 관리한다면
+ * 서버를 통해 게임을 진행시키는 것과 같고
+ * 클라이언트에서 그런 중요정보를 처리하지 않음으로써
+ * 메이플의 클라이언트 변조사건 등을 막을 수 있다.
+ */
+public class GamePlayer : NetworkBehaviour
+{
+
+}
